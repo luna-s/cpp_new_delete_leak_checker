3545712bdeca22f0616dc6d25336c71115bd16db
luna-s <lumina80@naver.com>
2017-04-05 13:54:26 +0900

    + heap over run checker

---
+++ newh2/test_main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <cstdio>
+#include "newh.h"
+
+void main()
+{
+	int *a = new int;
+	delete a;
+
+
+	float *b = new float;
+	delete b;
+
+	char *c = new char[10];
+	delete[] c;
+
+
+	std::string *d = new std::string;
+	delete d;
+
+	std::string *e = new std::string("korea");
+	delete e;
+
+
+
+	lunas::s_heap_manager.printInfoAll();
+
+	int *aa = new int;
+	float *bb = new float;
+	char *cc = new char[1000];
+
+	lunas::s_heap_manager.printInfoAll();
+
+	std::string *dd = new std::string();
+	std::string *ee = new std::string("korea");
+
+	lunas::s_heap_manager.printInfoAll();
+
+	delete aa;
+	delete bb;
+	delete[] cc;
+
+	lunas::s_heap_manager.printInfoAll();
+
+	delete dd;
+	delete ee;
+
+	lunas::s_heap_manager.printInfoAll();
+
+	int *aaa = new int;
+	float *bbb = new float;
+
+	lunas::s_heap_manager.printInfoAll();
+	lunas::s_heap_manager.checkHeapOverRun();
+
+
+}
